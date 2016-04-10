a1e0c97066639e13fe38c69318f07aff1c45a551
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-04-10 16:25:44 -0400

    graphs, etc

--- bernie360.js
+++ bernie360.js
@@ -11,15 +11,6 @@ function init() {
 
     var isPlaying = false;
 
-    function startVideo() {
-        if (!isPlaying) {
-            video.play();
-            animate();
-            isPlaying = true;
-            setTimeout(addGraphs, 5000);
-        }
-    }
-
     var container = document.getElementById( 'container' );
     container.addEventListener( 'click', function () {
         startVideo();
@@ -34,11 +25,16 @@ function init() {
     //video.loop = true;
     //video.buffered = true;
     video.crossOrigin = "anonymous";
-    //video.src = 'BernieStereoHighResBronx.mp4';
-    //video.src = 'bernie_stereo_1080_web_optimized.mp4';
-    video.src = 'bernie_stereo_1024.mp4';
-    //video.src = 'http://ec2-52-87-181-40.compute-1.amazonaws.com/videos/bernie_stereo_1024.mp4';
-    //video.src = 'http://ec2-52-87-181-40.compute-1.amazonaws.com/videos/BernieStereoHighResBronx.mp4';
+    // video.src = 'bernie_stereo_1024.mp4';
+    // video.src = 'http://ec2-52-87-181-40.compute-1.amazonaws.com/videos/bernie_stereo_1024.mp4';
+    // video.src = 'http://ec2-52-87-181-40.compute-1.amazonaws.com/videos/BernieStereoHighResBronx.mp4';
+    if (isMobile()) {
+        // lower res for mobile
+        video.src = 'bernie_stereo_1080_web_optimized.mp4';
+    } else {
+        // high res, video can autostart on desktop
+        video.src = 'BernieStereoHighResBronx.mp4';
+    }
 
     texture = new THREE.VideoTexture( video );
 
@@ -104,6 +100,10 @@ function init() {
         scene.add( mesh );
     } )();
 
+    var directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(50, 30, 20);
+    scene.add(directionalLight);
+
     //
 
     renderer = new THREE.WebGLRenderer();
@@ -121,8 +121,12 @@ function init() {
     effect.setSize( window.innerWidth, window.innerHeight );
 
     if ( WEBVR.isAvailable() === true ) {
-
-        document.body.appendChild( WEBVR.getButton( effect ) );
+        var button = WEBVR.getButton( effect );
+        // button.onclick = function () {
+        //     effect.setFullScreen(true);
+        //     controls.resetPose();
+        // }
+        document.body.appendChild( button );
 
     }
 
@@ -137,6 +141,18 @@ function init() {
 
         effect.setSize( window.innerWidth, window.innerHeight );
 
+    }
+
+    if (!isMobile()) startVideo();
+
+    function startVideo() {
+        if (!isPlaying) {
+            isPlaying = true;
+            video.play();
+            animate();
+            setTimeout(addBoxGraph, 5000);
+            setTimeout(addPieChart, 12000);
+        }
     }
 
     function animate() {
@@ -157,11 +173,9 @@ function init() {
 
     }
 
-    function addGraphs() {
 
-        var directionalLight = new THREE.DirectionalLight(0xffffff);
-        directionalLight.position.set(50, 30, 20);
-        scene.add(directionalLight);
+    function addBoxGraph() {
+        // bar / box graph
 
         var colors = [0xff0000, 0xff2200, 0xff4400, 0xff6600, 0xff8800];
         var boxMeshes = [];
@@ -170,7 +184,8 @@ function init() {
         for (var i = 0; i < colors.length; i++) {
             var boxMaterial = new THREE.MeshLambertMaterial({color: colors[i], transparent: true, opacity: 0});
             var boxMesh = new THREE.Mesh(boxGeom, boxMaterial);
-            boxMesh.position.set(-2 + i, 0.75, -3);
+            boxMesh.scale.set(1, 1 + i, 1);
+            boxMesh.position.set(-2 + i, 0.75 + 0.5*boxMesh.scale.y, -3);
             scene.add(boxMesh);
             boxMeshes.push(boxMesh);
         }
@@ -180,10 +195,61 @@ function init() {
             for (var i = 0; i < boxMeshes.length; i++) {
                 var boxMaterial = boxMeshes[i].material;
                 boxMaterial.opacity += 0.01 - 0.0001*i;
-                if (boxMaterial.opacity === 1) {
-                    clearInterval(fadeInInterval);
-                }
+                boxMaterial.opacity = Math.min(1, boxMaterial.opacity);
             }
+            if (boxMeshes[boxMeshes.length-1].material.opacity === 1) {
+                clearInterval(fadeInInterval);
+            }
+        }, 30);
+
+        // function fadeOut() {
+        //     for (var i = 0; i < boxMeshes.length; i++) {
+        //         var boxMaterial = boxMeshes[i].material;
+        //         boxMaterial.opacity -= (0.01 - 0.0001*i);
+        //         if (boxMaterial.opacity === 1) {
+        //             clearInterval(fadeOutInterval);
+        //         }
+        //     }
+        // }
+
+    }
+
+    function addPieChart() {
+        // pie charts
+
+        var colors = [0x008800, 0x00aa44, 0x00bb88];
+        var thetaLengths = [2*Math.PI / 2, 2*Math.PI / 4, 2*Math.PI / 4];
+
+        var sliceMeshes = [];
+
+        for (var i = 0; i < colors.length; i++) {
+            var sliceGeom = new THREE.CylinderBufferGeometry(0.75, // radius top
+                                                             0.75, // radius bottom
+                                                             0.15, // height
+                                                             12, // radius segments
+                                                             1, // height segments
+                                                             false, // open-ended
+                                                             thetaLengths.slice(0,i).reduce( (p, c) => p + c, 0 ), // start angle
+                                                             thetaLengths[i]); // angle swept
+            var sliceMaterial = new THREE.MeshLambertMaterial({color: colors[i], transparent: true, opacity: 0});
+            var sliceMesh = new THREE.Mesh(sliceGeom, sliceMaterial);
+            sliceMesh.rotation.x = 0.8 * Math.PI / 2;
+            sliceMesh.position.set(3.5, 1, -3);
+            sliceMesh.scale.set(0.85, 0.85, 0.85);
+            scene.add(sliceMesh);
+            sliceMeshes.push(sliceMesh);
+        }
+
+        var fadeInInterval;
+        fadeInInterval = setInterval( function () {
+            for (var i = 0; i < sliceMeshes.length; i++) {
+                var sliceMaterial = sliceMeshes[i].material;
+                sliceMaterial.opacity += 0.01 - 0.001 * i;
+                sliceMaterial.opacity = Math.min(1, sliceMaterial.opacity);
+            }
+            if (sliceMeshes[sliceMeshes.length-1].material.opacity === 1) {
+                clearInterval(fadeInInterval);
+            }                
         }, 30);
 
     }

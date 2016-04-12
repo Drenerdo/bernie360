db2135d079f04f47aa6460d4fde3892195a0fb88
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-04-11 21:56:23 -0400

    cleanup

--- bernie360.js
+++ bernie360.js
@@ -1,8 +1,4 @@
 function init() {
-    var camera, scene, renderer;
-    var video, texture;
-
-    var controls, effect;
 
     if ( WEBVR.isLatestAvailable() === false ) {
         console.warn( 'deprecated version of the WebVR API is being used' );
@@ -15,16 +11,23 @@ function init() {
         startVideo();
     } );
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 2000 );
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor( 0x101010 );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 2000 );
     camera.layers.enable( 1 ); // render left view when no stereo available
 
     // video
 
-    video = document.createElement( 'video' );
+    var video = document.createElement( 'video' );
+
     // video.loop = true;
-    // video.buffered = true;
+
     // video.crossOrigin = "anonymous";
-    // video.crossorigin = "anonymous";
+
     if (isMobile()) {
         // lower res for mobile
         video.src = '/static/bernie_stereo_1080_web_optimized.mp4';
@@ -35,7 +38,7 @@ function init() {
         video.src = '/static/BernieStereoHighResBronx.mp4';
     }
 
-    texture = new THREE.VideoTexture( video );
+    var texture = new THREE.VideoTexture( video );
 
     texture.minFilter = THREE.NearestFilter;
     texture.maxFilter = THREE.NearestFilter;
@@ -45,78 +48,52 @@ function init() {
     texture.format = THREE.RGBFormat;
     texture.generateMipmaps = false;
 
-    scene = new THREE.Scene();
+    var scene = new THREE.Scene();
 
-    // left
-
-    ( function () {
-        var geometry = new THREE.SphereGeometry( 500, 60 / 2, 40 / 2);
-        geometry.scale( 1, 1, 1 );
-
-        var uvs = geometry.faceVertexUvs[ 0 ];
-
-        for ( var i = 0; i < uvs.length; i ++ ) {
-
-            for ( var j = 0; j < 3; j ++ ) {
-
-                uvs[ i ][ j ].y *= 0.5;
-
-            }
-
-        }
-
-        var material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.BackSide } );
-
-        var mesh = new THREE.Mesh( geometry, material );
-        // mesh.rotation.y = Math.PI / 2;
-        mesh.layers.set( 1 ); // display in left eye only
-        scene.add( mesh );
-    } )();
-
-    // right
-    ( function () {
-        var geometry = new THREE.SphereGeometry( 500, 60 / 2, 40 / 2);
-        geometry.scale( 1, 1, 1 );
-
-        var uvs = geometry.faceVertexUvs[ 0 ];
-
-        for ( var i = 0; i < uvs.length; i ++ ) {
-
-            for ( var j = 0; j < 3; j ++ ) {
-
-                uvs[ i ][ j ].y *= 0.5;
-                uvs[ i ][ j ].y += 0.5;
-
-            }
-
-        }
-
-        var material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.BackSide } );
-
-        var mesh = new THREE.Mesh( geometry, material );
-        //mesh.rotation.y = - Math.PI / 2;
-        mesh.layers.set( 2 ); // display in right eye only
-        scene.add( mesh );
-    } )();
-
+    // this light illuminates the 3D charts
     var directionalLight = new THREE.DirectionalLight(0xffffff);
     directionalLight.position.set(50, 30, 20);
     scene.add(directionalLight);
 
-    //
+    ( function () {
+        // create video sphere for left eye
+        var geometry = new THREE.SphereGeometry( 500, 60 / 2, 40 / 2);
+        geometry.scale( 1, 1, 1 );
+        var uvs = geometry.faceVertexUvs[ 0 ];
+        for ( var i = 0; i < uvs.length; i ++ ) {
+            for ( var j = 0; j < 3; j ++ ) {
+                uvs[ i ][ j ].y *= 0.5;
+            }
+        }
+        var material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.BackSide } );
+        var mesh = new THREE.Mesh( geometry, material );
+        mesh.rotation.y = Math.PI / 2;
+        mesh.layers.set( 1 ); // display in left eye only
+        scene.add( mesh );
+    } )();
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor( 0x101010 );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    container.appendChild( renderer.domElement );
+    ( function () {
+        // create video sphere for right eye
+        var geometry = new THREE.SphereGeometry( 500, 60 / 2, 40 / 2);
+        geometry.scale( 1, 1, 1 );
+        var uvs = geometry.faceVertexUvs[ 0 ];
+        for ( var i = 0; i < uvs.length; i ++ ) {
+            for ( var j = 0; j < 3; j ++ ) {
+                uvs[ i ][ j ].y *= 0.5;
+                uvs[ i ][ j ].y += 0.5;
+            }
+        }
+        var material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.BackSide } );
+        var mesh = new THREE.Mesh( geometry, material );
+        mesh.rotation.y = Math.PI / 2;
+        mesh.layers.set( 2 ); // display in right eye only
+        scene.add( mesh );
+    } )();
 
-    //
+    var controls = new THREE.VRControls( camera );
 
-    controls = new THREE.VRControls( camera );
-
-    effect = new THREE.VREffect( renderer );
-    effect.scale = 0; // video doesn't need eye separation
+    var effect = new THREE.VREffect( renderer );
+    // effect.scale = 0; // video doesn't need eye separation
     effect.setSize( window.innerWidth, window.innerHeight );
 
     if ( WEBVR.isAvailable() === true ) {
@@ -126,7 +103,6 @@ function init() {
         //     controls.resetPose();
         // }
         document.body.appendChild( button );
-
     }
 
     //
@@ -134,12 +110,9 @@ function init() {
     window.addEventListener( 'resize', onWindowResize, false );
 
     function onWindowResize() {
-
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
-
         effect.setSize( window.innerWidth, window.innerHeight );
-
     }
 
     if (!isMobile()) startVideo();
@@ -155,21 +128,16 @@ function init() {
     }
 
     function animate() {
-
         requestAnimationFrame( animate );
         render();
-
     }
 
     function render() {
-
         controls.update();
         effect.render( scene, camera );
-
     }
 
     function displayText(text) {
-
     }
 
 

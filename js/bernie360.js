079490b0a7f587e5d603901eea52d14dac18314a
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2016-04-15 18:42:40 -0400

    experiment w/ loading scene - soon to be reverted, i dont think it works well

--- js/bernie360.js
+++ js/bernie360.js
@@ -3,8 +3,11 @@ const COLORS = {
 };
 
 
-var BERNIE = {
-    loadingManager: new THREE.LoadingManager()
+var BERNIE360 = {
+    loadingManager: new THREE.LoadingManager(),
+    fonts: {},
+    animateLoadingCallbacks: [],
+    animateCallbacks: []
 };
 
 
@@ -39,6 +42,7 @@ function init() {
     }
 
     var scene = new THREE.Scene();
+    var loadingScene = new THREE.Scene();
 
     // TODO: potential optimization to try later:
     // scene.autoUpdate = false;
@@ -72,7 +76,8 @@ function init() {
         //video.src = 'http://ec2-52-87-181-40.compute-1.amazonaws.com/videos/BernieStereoHighResBronx.mp4';
         //video.src = '/static/BernieStereoHighResBronx.mp4';
         //video.src = '/static/video/bernie_stereo_2160_web_optimized.mp4';
-        video.src = '/static/video/bernie_stereo_2160.webm'; // encoded w/ VP8 instead of H.264, works in the WebVR Chrome builds!
+        //video.src = '/static/video/bernie_stereo_2160.webm'; // encoded w/ VP8 instead of H.264, works in the WebVR Chrome builds!
+        video.src = '/static/video/wsp_stereo_2160.mp4';
     }
     var texture = new THREE.VideoTexture( video );
     texture.minFilter = THREE.NearestFilter;
@@ -105,7 +110,7 @@ function init() {
         leftVideoSphere.rotation.y = -Math.PI / 2;
         leftVideoSphere.updateMatrix();
         leftVideoSphere.layers.set( 1 ); // display in left eye only
-        leftVideoSphere.visible = false;
+        // leftVideoSphere.visible = false;
         scene.add( leftVideoSphere );
     } )();
     var rightVideoSphere;
@@ -128,14 +133,14 @@ function init() {
         rightVideoSphere.rotation.y = -Math.PI / 2;
         rightVideoSphere.updateMatrix();
         rightVideoSphere.layers.set( 2 ); // display in right eye only
-        rightVideoSphere.visible = false;
+        // rightVideoSphere.visible = false;
         scene.add( rightVideoSphere );
     } )();
 
     var isPlaying = false;
     function startVideo() {
-        leftVideoSphere.visible = true;
-        rightVideoSphere.visible = true;
+        // leftVideoSphere.visible = true;
+        // rightVideoSphere.visible = true;
         if (!isPlaying) {
             isPlaying = true;
             video.play();
@@ -146,13 +151,21 @@ function init() {
     // set callback for when everything is loaded / async requests have completed:
     // ********************************************************************************************
 
-    BERNIE.loadingManager.onLoad = function () {
+    //var animateLoadingRequestID;
+
+    const tCountdown = 5000; // set for a 5 second countdown after everything is loaded
+    var tStart;
+    BERNIE360.loadingManager.onLoad = function () {
+        // // stop animating the loading scene and start animating the real scene:
+        // cancelAnimationFrame(animateLoadingRequestID);
+        // requestAnimationFrame(animate);
+        tStart = lt + tCountdown;
         if (!isMobile()) {
-            startVideo();
+            setTimeout(startVideo, tCountdown);
         } else {
             // TODO: cardboard viewer selection
-            // TODO: video cannot autoplay on Android, so there has to be some prompt to touch the screen
-            //       or perform some action to start the video
+            // TODO: video cannot autoplay on Android, there has to be some prompt to touch the screen
+            //       or similar action to start playing the video
             // var mesh = makeTextMesh('Touch to begin!');
             // mesh.position.set(0, 0, -2);
             // mesh.updateMatrix();
@@ -162,9 +175,10 @@ function init() {
                 //scene.remove(mesh);
             });
         }
+
     };
 
-    BERNIE.loadingManager.onProgress = function (url, nLoaded, nTotal) {
+    BERNIE360.loadingManager.onProgress = function (url, nLoaded, nTotal) {
         // TODO: implement some loading progress indicator
     };
 
@@ -172,61 +186,120 @@ function init() {
     // setup charts / visualizations:
     // ********************************************************************************************
 
-    // this light illuminates the 3D charts
+    // this light illuminates the 3D objects:
     var directionalLight = new THREE.DirectionalLight(0xffffff);
     directionalLight.position.set(20, 30, 50);
     directionalLight.updateMatrix();
     scene.add(directionalLight);
 
-    var lineAreaChart = makeLineAreaChart(INCOME_INEQUALITY.x, INCOME_INEQUALITY.y, {
-        width: 4,
-        height: 2,
-        depth: 0.2,
-        yMin: 0,
-        titleImage: '/static/img/inequality_title.png',
-        xLabels: [1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010].map( (year) => '/static/img/income_inequality/' + year + '.png' ),
-        yLabels: ['0', '6.25', '12.5', '18.75', '25'].map( (filename) => '/static/img/income_inequality/' + filename + '.png' ),
-        areaMaterial: new THREE.MeshPhongMaterial({color: COLORS.chart_background, shininess: 60})
-    }, function (chart) {
-        chart.position.set(-2, 2.5, -4);
-        chart.updateMatrix();
-        scene.add(chart);
+    var fontLoadingManager = new THREE.LoadingManager( function () {
 
-        BERNIE.loadingManager.onLoad();
-    });
+        // this callback is executed once all fonts are loaded
 
-    var barChart = makeBarChart([0.2, 0.6, 1, 0.8, 0.4], {
-        barWidth: 0.25,
-        barDepth: 0.25,
-        barSeparation: 0.05,
-        barMaterial: new THREE.MeshPhongMaterial({color: 0xff3333})
-    });
-    barChart.rotation.z = -Math.PI / 2;
-    barChart.position.set(-4, 4, -3);
-    barChart.updateMatrix();
-    scene.add(barChart);
+        // text geometry example:
+        var textGeom = new THREE.TextGeometry("BERNIE 360!", {
+            font: BERNIE360.fonts.helvetiker_bold,
+            size: 0.3,
+            //curveSegments: 4,
+            height: 0.05
+        });
+        textGeom.center();
+        var material = new THREE.MeshPhongMaterial({color: 0x44ee44, shininess: 75});
+        var mesh = new THREE.Mesh(textGeom, material);
+        mesh.position.set(0, 1, -3);
+        mesh.updateMatrix();
+        loadingScene.add(mesh);
+        BERNIE360.animateLoadingCallbacks.push( function (t, dt) {
+            mesh.rotation.y += 2 * dt;
+            mesh.updateMatrix();
+        } );
+
+        // add example bar chart:
+        var barChart = makeBarChart([0.2, 0.6, 1, 0.8, 0.4], {
+            barWidth: 0.25,
+            barDepth: 0.25,
+            barSeparation: 0.05,
+            barMaterial: new THREE.MeshPhongMaterial({color: 0xff3333})
+        });
+        barChart.rotation.z = -Math.PI / 2;
+        barChart.position.set(-4, 4, -3);
+        barChart.updateMatrix();
+        scene.add(barChart);
+
+        // add example pie chart:
+        // TODO
+
+        // add example line area chart:
+        var lineAreaChart = makeLineAreaChart(INCOME_INEQUALITY.x, INCOME_INEQUALITY.y, {
+            width: 4,
+            height: 2,
+            depth: 0.2,
+            yMin: 0,
+            titleImage: '/static/img/inequality_title.png',
+            xLabels: [1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010].map( (year) => '/static/img/income_inequality/' + year + '.png' ),
+            yLabels: ['0', '6.25', '12.5', '18.75', '25'].map( (filename) => '/static/img/income_inequality/' + filename + '.png' ),
+            //areaMaterial: new THREE.MeshPhongMaterial({color: COLORS.chart_background, shininess: 60})
+            areaMaterial: new THREE.MeshLambertMaterial({color: COLORS.chart_background})
+        }, function (chart) {
+            chart.position.set(-2, 2.5, -4);
+            chart.updateMatrix();
+            scene.add(chart);
+
+            BERNIE360.loadingManager.onLoad();
+        });
+
+    } );
+
+    var fontLoader = new THREE.FontLoader(fontLoadingManager);
+
+    // load the fonts that are included with three.js:
+    ['gentilis_bold', 'helvetiker_bold', 'optimer_bold'].forEach( function (fontName) {
+        fontLoader.load('/static/node_modules/three/examples/fonts/' + fontName + '.typeface.js', function (font) {
+            BERNIE360.fonts[fontName] = font;
+        });
+    } );
+    ['droid_sans_bold', 'droid_sans_mono_regular', 'droid_serif_bold'].forEach( function (fontName) {
+        fontLoader.load('/static/node_modules/three/examples/fonts/droid/' + fontName + '.typeface.js', function (font) {
+            BERNIE360.fonts[fontName] = font;
+        });
+    } );
 
     // ********************************************************************************************
-    // start rendering:
+    // start rendering the loading scene:
     // ********************************************************************************************
 
-    requestAnimationFrame( animate );
-
+    //animateLoadingRequestID = requestAnimationFrame( animateLoading );
+    requestAnimationFrame( animateLoading );
 
     var lt = 0;
-    function animate(t) {
+    function animateLoading(t) {
         var dt = (t - lt) * 0.001;
-        requestAnimationFrame( animate );
-        render();
+        for (var i = 0, l = BERNIE360.animateLoadingCallbacks.length; i < l; i++) {
+            BERNIE360.animateLoadingCallbacks[i](t, dt);
+        }
+        vrControls.update();
+        vrEffect.render( loadingScene, camera );
+        if (tStart && t >= tStart) {
+            // start animating the real scene:
+            video.currentTime = 0;
+            requestAnimationFrame(animate);
+        } else {
+            //animateLoadingRequestID = requestAnimationFrame( animateLoading );
+            requestAnimationFrame( animateLoading );
+        }
         lt = t;
     }
 
-
-    function render() {
+    function animate(t) {
+        var dt = (t - lt) * 0.001;
+        requestAnimationFrame( animate );
+        for (var i = 0, l = BERNIE360.animateCallbacks.length; i < l; i++) {
+            BERNIE360.animateCallbacks[i](t, dt);
+        }
         vrControls.update();
         vrEffect.render( scene, camera );
+        lt = t;
     }
-
 
     // uncomment to view mesh wireframes:
     // var wireframeMaterial = new THREE.MeshBasicMaterial({color: 0xeeddaa, wireframe: true});
